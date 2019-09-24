24f45d655a4b65f5398456c756c48f0c1830adfd
Claire <clairepelletier.prod@gmail.com>
2019-09-24 15:41:39 +0200

    Refactor function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -3,13 +3,11 @@ const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirstLetters(input) {
-  output = '';
+  let output = '';
   for (let i = 0; i < input.length; i++) {
-    if (i === 0 || input[i - 1] === ' ') {
-      output += input[i].toUpperCase();
-    } else {
-      output += input[i];
-    }
+    (i === 0 || input[i - 1] === ' ')
+    ? output += input[i].toUpperCase()
+    : output += input[i];
   }
   return output;
 }
