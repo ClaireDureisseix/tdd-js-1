979063721ee5cb3c73b5fd4ce5af2db34587b895
Claire <clairepelletier.prod@gmail.com>
2019-09-24 13:35:20 +0200

    Implement function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,6 +2,17 @@
 const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
+function capitalizeFirstLetters(input) {
+  output = '';
+  for (let i = 0; i < input.length; i++) {
+    if (i === 0 || input[i - 1] === ' ') {
+      output += input[i].toUpperCase();
+    } else {
+      output += input[i];
+    }
+  }
+  return output;
+}
 
 // Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
