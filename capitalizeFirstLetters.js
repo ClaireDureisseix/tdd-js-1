0fb6370d00e7824a69a9eaeab8792998395c8b93
Claire <clairepelletier.prod@gmail.com>
2019-09-25 11:54:43 +0200

    git tag step-2

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,4 @@
 // capitalizeFirstLetters.js
-const assert = require('assert');
-
-// WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirstLetters(input) {
   let output = '';
   for (let i = 0; i < input.length; i++) {
@@ -12,15 +9,4 @@ function capitalizeFirstLetters(input) {
   return output;
 }
 
-// Check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-// Check that capitalizeFirstLetters accepts one argument
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-// Check that capitalizeFirstLetters transforms a sentence correctly
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
-// Check that capitalizeFirstLetters transforms javaScript correctly
-assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
-// Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirstLetters(''), '');
+module.exports = capitalizeFirstLetters;
